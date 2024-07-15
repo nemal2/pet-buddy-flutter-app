eab8be66c58085d5b181eab6ed56fa9a54036276
Nemal Perera <122453475+nemal2@users.noreply.github.com>
2024-07-15 10:38:52 +0530

    test commit

--- lib/utils/main.dart
+++ lib/utils/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+        //test git
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
